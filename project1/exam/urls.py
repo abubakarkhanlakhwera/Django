885365ae7a3e61017a47266674d79de41041bced
lakhwera_001 <abubakarkhanlakhwara@gmail.com>
2024-08-08 08:25:04 +0500

    d

--- project1/exam/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path 
-from exam import views
-
-urlpatterns = [
-    path("testpaper", views.testpaper),
-    path("result",views.result),
-]
--- project1/testapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from testapp import views
-urlpatterns = [
-    path("hello",views.greeting),
-    path("about",views.about),
-    path("contact",views.contact),
-]
