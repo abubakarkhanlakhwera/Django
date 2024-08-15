711632eab44c61121d283b5cbe7ef709f0e1dc9f
lakhwera_001 <abubakarkhanlakhwara@gmail.com>
2024-08-15 09:08:55 +0500

    new commit

--- myproject3/d1/urls.py
+++ myproject3/d1/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index),  # This maps the root URL to the index view
     path('counter',views.counter),
+    path('register', views.register),
+    path('login',views.login),
+    path('post/<str:id>',views.post)
 ]
--- myproject3/demo1/settings.py
+++ myproject3/demo1/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'demo1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'demo1',
+        'USER': 'postgres',
+        'PASSWORD': 'bobo',
+        'HOST': 'localhost',
+        'PORT': '4321',
     }
 }
 
