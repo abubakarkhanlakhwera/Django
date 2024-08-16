2901d0b4548efb425af468148a17e552f3524431
lakhwera_001 <abubakarkhanlakhwara@gmail.com>
2024-08-16 10:51:30 +0500

    new commit

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -4,3 +4,7 @@ from .models import Post
 def index(req):
     posts = Post.objects.all()
     return render(req,'index.html', {'posts':posts})
+
+def post(req,id):
+    posts = Post.objects.get(id=id)
+    return render(req,'post.html',{'posts':posts})
